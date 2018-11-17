8e29d50fecc76b2fc657b72025f8e55441530533
MashaBut <42973147+MashaBut@users.noreply.github.com>
2018-11-17 20:12:16 +0200

    snake without class

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace snake
+{
+    partial class Tail
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tail));
+            this.Speed = new System.Windows.Forms.Label();
+            this.Mark = new System.Windows.Forms.Label();
+            this.Fuild = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textMark = new System.Windows.Forms.Label();
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Fuild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Speed
+            // 
+            this.Speed.AutoSize = true;
+            this.Speed.BackColor = System.Drawing.Color.Orange;
+            this.Speed.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.Speed.Location = new System.Drawing.Point(29, 40);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(0, 36);
+            this.Speed.TabIndex = 0;
+            this.Speed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Mark
+            // 
+            this.Mark.AutoSize = true;
+            this.Mark.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Mark.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.Mark.ForeColor = System.Drawing.Color.White;
+            this.Mark.Image = ((System.Drawing.Image)(resources.GetObject("Mark.Image")));
+            this.Mark.Location = new System.Drawing.Point(35, 40);
+            this.Mark.Name = "Mark";
+            this.Mark.Size = new System.Drawing.Size(96, 36);
+            this.Mark.TabIndex = 1;
+            this.Mark.Text = "Очки:";
+            // 
+            // Fuild
+            // 
+            this.Fuild.BackColor = System.Drawing.Color.Snow;
+            this.Fuild.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Fuild.BackgroundImage")));
+            this.Fuild.Location = new System.Drawing.Point(30, 101);
+            this.Fuild.Name = "Fuild";
+            this.Fuild.Size = new System.Drawing.Size(640, 640);
+            this.Fuild.TabIndex = 3;
+            this.Fuild.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textMark
+            // 
+            this.textMark.AutoSize = true;
+            this.textMark.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textMark.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.textMark.ForeColor = System.Drawing.Color.White;
+            this.textMark.Image = ((System.Drawing.Image)(resources.GetObject("textMark.Image")));
+            this.textMark.Location = new System.Drawing.Point(181, 40);
+            this.textMark.MinimumSize = new System.Drawing.Size(100, 36);
+            this.textMark.Name = "textMark";
+            this.textMark.Size = new System.Drawing.Size(100, 36);
+            this.textMark.TabIndex = 4;
+            this.textMark.Text = "0";
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.TimerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.TimerLabel.ForeColor = System.Drawing.Color.White;
+            this.TimerLabel.Image = ((System.Drawing.Image)(resources.GetObject("TimerLabel.Image")));
+            this.TimerLabel.Location = new System.Drawing.Point(570, 40);
+            this.TimerLabel.MinimumSize = new System.Drawing.Size(100, 36);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(100, 36);
+            this.TimerLabel.TabIndex = 4;
+            this.TimerLabel.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(412, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Время:";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Tail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(682, 753);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.textMark);
+            this.Controls.Add(this.Fuild);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Mark);
+            this.Controls.Add(this.Speed);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForeColor = System.Drawing.Color.Aqua;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(700, 800);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(700, 600);
+            this.Name = "Tail";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SNAKE";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Tail_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Fuild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Speed;
+        private System.Windows.Forms.Label Mark;
+        private System.Windows.Forms.PictureBox Fuild;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label textMark;
+        private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace snake
+{
+    public partial class Tail : Form
+    {
+        SoundPlayer sp;//пожирание еды
+        SoundPlayer exit;//врезание в стенку
+        List<Rectangle> snake = new List<Rectangle>();//snake
+        public int lenghtSnake;// long snake
+        public enum Course { UP, DOWN, LEFT, RIGHT };
+        public Course course;// направление движения
+        public int addX, addY;//смещение координат
+        Rectangle food = new Rectangle();//fuild foog
+        List<Rectangle> empty_filed = new List<Rectangle>();//free point
+        public Random rand = new Random();
+        public bool fruit;
+        int time;
+        int mark;
+        int t;
+        public Tail()
+        {
+            InitializeComponent();
+            sp = new SoundPlayer(@"sound.wav");
+            exit = new SoundPlayer(@"mario.wav");
+            message();
+        }
+
+        //snake
+        private void DrawSnake()
+        {
+            Graphics graphics = Fuild.CreateGraphics();
+            for (int i = 0; i < snake.Count; i++)
+            {
+                if (i == 0)
+                {
+                    graphics.FillEllipse(Brushes.Black, snake[i]);
+                }
+                else
+                {
+                    graphics.FillEllipse(Brushes.Green, snake[i]);
+                }
+            }
+        }
+
+        //fruit
+        private void DrawFruit()
+        {
+            if (!fruit)
+            {
+                f:
+                for (int i = 0; i < 13; i++)
+                {
+                    for (int j = 0; j < 13; j++)
+                    {
+                        Rectangle point = new Rectangle(i * 20, j * 20, 20, 20);
+                        if (snake.IndexOf(point) == -1)
+                        {
+                            empty_filed.Add(point);
+                        }
+                    }
+                }
+                food = empty_filed[rand.Next(0, empty_filed.Count - 1)];
+                for(int i=0;i<snake.Count;i++)
+                {
+                    if (snake[i] == food)
+                    {
+                        goto f;
+                    } 
+                }
+                empty_filed.Clear();
+                Graphics g = Fuild.CreateGraphics();
+                g.FillEllipse(Brushes.Red, food);
+                fruit = true;
+            }
+            else
+            {
+                Graphics g = Fuild.CreateGraphics();
+                g.FillEllipse(Brushes.Red, food);
+            }
+        }
+
+        //последующие игры
+        private void message()
+        {
+            time = 0;
+            t = 0;
+            TimerLabel.Text = t.ToString();
+            mark = 0;
+            textMark.Text = mark.ToString();
+            Refresh();
+            snake.Clear();
+            empty_filed.Clear();
+            course = Course.RIGHT;
+            snake.Add(new Rectangle(320, 320, 20, 20));
+            snake.Add(new Rectangle(300, 320, 20, 20));
+            snake.Add(new Rectangle(280, 320, 20, 20));
+            timer1.Interval = 210;
+            lenghtSnake = 3;
+            fruit = false;
+            DrawSnake();
+            DrawFruit();
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+        }
+
+        //ошибка змеи
+        private void erorr()
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            exit.Play();
+            DialogResult result1 = MessageBox.Show(
+                 "Желаете начать игру заново?", "Ой!",
+                 MessageBoxButtons.OKCancel,
+                 MessageBoxIcon.Question,
+                 MessageBoxDefaultButton.Button1
+                 );
+            if (result1 == DialogResult.OK)
+            {
+                message();
+            }
+            else if (result1 == DialogResult.Cancel)
+            {
+                Close();
+                return;
+            }
+        }
+
+        private  void drove()
+        {   Refresh();
+            if (course == Course.UP)
+            {
+                addX = 0;
+                addY = -20;
+            }
+            if (course == Course.DOWN)
+            {
+                addX = 0;
+                addY = 20;
+            }
+            if (course == Course.LEFT)
+            {
+                addX = -20;
+                addY = 0;
+            }
+            if (course == Course.RIGHT)
+            {
+                addX = 20;
+                addY = 0;
+            }
+            Rectangle prev_segment = snake[0];
+            Rectangle next_segment;
+            for (int i = 0; i < snake.Count - 1; i++)
+            {
+                if (i == 0)
+                {
+                    snake[i] = new Rectangle(snake[i].X + addX, snake[i].Y + addY, 20, 20);
+                }
+                if (!(snake[i + 1].IsEmpty))
+                {
+                    next_segment = snake[i + 1];
+                    snake[i + 1] = prev_segment;
+                    prev_segment = next_segment;
+                }
+            }
+            if (snake[0] == food)
+            {
+                snake.Add(food);
+                lenghtSnake++;
+                mark += 10;
+                textMark.Text = Convert.ToString(mark);
+                sp.Play();
+                if (lenghtSnake % 7 == 0)
+                {if (timer1.Interval >= 30)
+                    {
+                        timer1.Interval -= 30;
+                    }
+                }
+                fruit = false;
+            }
+            for (int i = 1; i < snake.Count - 1; i++)
+            {
+                if (snake[0] == snake[i])
+                {
+                    erorr();
+                }
+            }
+            if ((snake[0].X < 0 || snake[0].X > 460 || snake[0].Y < 0 || snake[0].Y > 500))
+            {
+                erorr();
+            }
+            DrawSnake();
+            DrawFruit();
+        }
+        //время движения змейки
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            drove();
+        }
+        //время игры
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            t++;
+            TimerLabel.Text = t.ToString();
+        }
+        //работа клавиш
+        private void Tail_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up && course != Course.DOWN)
+            {
+                course = Course.UP;
+            }
+            if (e.KeyCode == Keys.Down && course != Course.UP)
+            {
+                course = Course.DOWN;
+            }
+            if (e.KeyCode == Keys.Left && course != Course.RIGHT)
+            {
+                course = Course.LEFT;
+            }
+            if (e.KeyCode == Keys.Right && course != Course.LEFT)
+            {
+                course = Course.RIGHT;
+            }
+        }
+    }
+}
